38185b356c176fa1042c4de3b794582fed750e7c
3m1lyk <kirkwood.emilyanne@gmail.com>
2026-04-15 02:48:34 -0400

    changed output display

--- Streamlit/pages/demo.py
+++ Streamlit/pages/demo.py
@@ -4,6 +4,8 @@ import pandas as pd
 model_weights = pd.read_csv("ridge_weights_skl.csv")
 mean_std_list = pd.read_csv("x_means_stds")
 intercept = 1.087586
+price_ratio = 0
+sale_price = 0
 
 w_month = model_weights["SaleMonth"]
 w_sqr_ftg = model_weights["TotalFinishedArea"]
@@ -23,10 +25,11 @@ sqr_ftg = (sqr_ftg - sqr_ftg_stats[0])/sqr_ftg_stats[1]
 
 
 price_ratio = intercept + (w_month * month) + (w_sqr_ftg * sqr_ftg)
-
-st.write("Price Ratio:", price_ratio)
+price_ratio = price_ratio.iloc[0]
+price_ratio = round(price_ratio, 2)
+st.write("Estimated Price Ratio:", price_ratio)
 
 appraised_val = st.number_input("Total Appraised Value ($):", min_value=0.0, format="%.2f")
 sale_price = price_ratio * appraised_val
+st.write("Estimated Sale Price", sale_price)
 
-st.write("Estimated Sale Price:", sale_price)
